4324acc816e87023ee82be0a1fc9d2ed978caded
ikkoyeah <s231624@studenti.polito.it>
2017-03-31 23:58:03 +0200

    Aggiunto layout profilo personale

--- app/src/main/java/it/polito/mad/team19/mad_expenses/MeActivity.java
+++ app/src/main/java/it/polito/mad/team19/mad_expenses/MeActivity.java
@@ -2,6 +2,13 @@ package it.polito.mad.team19.mad_expenses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import it.polito.mad.team19.mad_expenses.Adapters.MeRecyclerAdapter;
+import it.polito.mad.team19.mad_expenses.Classes.Me;
 
 public class MeActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me);
+        getSupportActionBar().setTitle(getResources().getString(R.string.personal_profile));
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        ArrayList<Me> me = new ArrayList<Me>();
+
+
+        for(int i = 0; i<4; i++) {
+            Me e = new Me("Nome", Integer.valueOf(1000).floatValue());
+            me.add(e);
+        }
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.fromto_rv);
+        MeRecyclerAdapter adapter = new MeRecyclerAdapter(this, me);
+        mRecyclerView.setAdapter(adapter);
+
+        LinearLayoutManager mLinearLayoutManagerVertical = new LinearLayoutManager(this);
+        mLinearLayoutManagerVertical.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mLinearLayoutManagerVertical);
+
     }
 }
