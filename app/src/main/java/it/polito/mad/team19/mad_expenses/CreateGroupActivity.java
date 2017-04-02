9b9ea316da2673214691998fa7c0cc7ca5da9f56
RizzoV <valentino.rizzo.93@gmail.com>
2017-04-03 00:29:44 +0200

    Inizio implementazione layout per creazione gruppo

---
+++ app/src/main/java/it/polito/mad/team19/mad_expenses/CreateGroupActivity.java
@@ -0,0 +1,13 @@
+package it.polito.mad.team19.mad_expenses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group_frag2);
+    }
+}
