3e2d2d14409f0e131bb88041a69a4e70125ec10a
RizzoV <valentino.rizzo.93@gmail.com>
2017-04-18 18:18:11 +0200

    Completato layout creazione gruppo + internazionalizzazione stringhe

--- app/src/main/java/it/polito/mad/team19/mad_expenses/CreateGroupActivity.java
+++ app/src/main/java/it/polito/mad/team19/mad_expenses/CreateGroupActivity.java
@@ -1,18 +1,10 @@
 package it.polito.mad.team19.mad_expenses;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -25,7 +17,6 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateGroupActivity extends AppCompatActivity {
 
-    Button info_type_button;
     CheckBox distributed;
     CheckBox centralized;
     Button add_group;
@@ -45,16 +36,11 @@ public class CreateGroupActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Crea un nuovo gruppo");
 
         mAuth = FirebaseAuth.getInstance();
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
-
-
-
         Log.e("User",mAuth.getCurrentUser().getUid());
 
-        distributed = (CheckBox) findViewById(R.id.type_1);
-        centralized = (CheckBox) findViewById(R.id.type_2);
-        info_type_button = (Button) findViewById(R.id.info_type_button);
+        distributed = (CheckBox) findViewById(R.id.distributed_checkbox);
+        centralized = (CheckBox) findViewById(R.id.centralized_checkbox);
         add_group = (Button) findViewById(R.id.add_group_submit);
         group_name = (EditText) findViewById(R.id.new_group_name);
 
@@ -97,15 +83,6 @@ public class CreateGroupActivity extends AppCompatActivity {
 
             }
         });
-
-        info_type_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(CreateGroupActivity.this,GroupTypeInfoActivity.class);
-                startActivity(intent);
-            }
-        });
-
 
         distributed.setOnClickListener(new View.OnClickListener() {
             @Override
