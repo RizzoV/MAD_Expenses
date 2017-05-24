cf7355c2b979fc68a6e6b522c2f2af85f267f0e2
ikkoyeah <s231624@studenti.polito.it>
2017-05-24 17:59:22 +0200

    Aggiunti valori alle notifiche

--- app/src/main/java/it/polito/mad/team19/mad_expenses/Classes/NotificationService.java
+++ app/src/main/java/it/polito/mad/team19/mad_expenses/Classes/NotificationService.java
@@ -164,7 +164,7 @@ public class NotificationService extends IntentService{
 
 
                                                                         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mContext);
-                                                                        mBuilder.setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 });
+                                                                        mBuilder.setVibrate(new long[] { 500, 500 });
                                                                         mBuilder.setLights(Color.parseColor(getString(R.color.colorPrimary)), 3000, 3000);
                                                                         //Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
                                                                         Uri alarmSound = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.notitification);
