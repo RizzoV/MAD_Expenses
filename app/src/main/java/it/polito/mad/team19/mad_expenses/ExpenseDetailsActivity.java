2c7e7eea8e465118a2eaa3db4c978251e9e7c5c1
RizzoV <valentino.rizzo.93@gmail.com>
2017-06-02 21:25:55 +0200

    Immagine fullscreen anche in proposte + Miglioramenti

--- app/src/main/java/it/polito/mad/team19/mad_expenses/ExpenseDetailsActivity.java
+++ app/src/main/java/it/polito/mad/team19/mad_expenses/ExpenseDetailsActivity.java
@@ -51,7 +51,6 @@ import it.polito.mad.team19.mad_expenses.Classes.FirebaseGroupMember;
 import it.polito.mad.team19.mad_expenses.Classes.NetworkChangeReceiver;
 import it.polito.mad.team19.mad_expenses.NotActivities.AsyncCurrencyConverter;
 import uk.co.senab.photoview.PhotoView;
-import uk.co.senab.photoview.PhotoViewAttacher;
 
 //TODO Jured: aggiungi click sulla tab History
 
@@ -86,7 +85,7 @@ public class ExpenseDetailsActivity extends AppCompatActivity {
 
     NetworkChangeReceiver netChange;
     IntentFilter filter;
-    Dialog nagDialog;
+    Dialog fullscreenImageDialog;
 
     private boolean zoomOut = false;
 
@@ -146,31 +145,32 @@ public class ExpenseDetailsActivity extends AppCompatActivity {
 
         // Click listener on the image
         if (imgUrl != null) {
-            nagDialog = new Dialog(ExpenseDetailsActivity.this, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
-            nagDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-            nagDialog.setCancelable(true);
-            nagDialog.setContentView(R.layout.expense_image_preview);
-            Button btnClose = (Button) nagDialog.findViewById(R.id.btnIvClose);
-            PhotoView ivPreview = (PhotoView) nagDialog.findViewById(R.id.iv_preview_image);
+            fullscreenImageDialog = new Dialog(ExpenseDetailsActivity.this, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+            fullscreenImageDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            fullscreenImageDialog.setCancelable(true);
+            fullscreenImageDialog.setContentView(R.layout.expense_image_fullscreen);
+            Button btnClose = (Button) fullscreenImageDialog.findViewById(R.id.btnIvClose);
+            PhotoView ivPreview = (PhotoView) fullscreenImageDialog.findViewById(R.id.iv_fullscreen_image);
 
             Glide.with(this).load(imgUrl).asBitmap().error(R.drawable.circle).into(new BitmapImageViewTarget(ivPreview) {
                 @Override
                 protected void setResource(Bitmap resource) {
                     ivPreview.setImageBitmap(resource);
+                    fullscreenImageDialog.findViewById(R.id.fullscreen_loading_tv).setVisibility(View.GONE);
                 }
             });
 
             btnClose.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View arg0) {
-                    nagDialog.dismiss();
+                    fullscreenImageDialog.dismiss();
                 }
             });
 
             expense_img.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    nagDialog.show();
+                    fullscreenImageDialog.show();
                 }
             });
         }
@@ -550,9 +550,9 @@ public class ExpenseDetailsActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
 
-        if (nagDialog != null)
-            if (nagDialog.isShowing())
-                nagDialog.dismiss();
+        if (fullscreenImageDialog != null)
+            if (fullscreenImageDialog.isShowing())
+                fullscreenImageDialog.dismiss();
 
         if (alertDialog != null)
             if (alertDialog.isShowing())
