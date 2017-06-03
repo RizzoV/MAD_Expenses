757a6a8bb4e97659c8c0c8b1f6be43a8f7980dfe
RizzoV <valentino.rizzo.93@gmail.com>
2017-06-04 01:32:50 +0200

    Internazionalizzazione SOLO XML

--- app/src/test/java/it/polito/mad/team19/mad_expenses/ExampleUnitTest.java
+++ app/src/test/java/it/polito/mad/team19/mad_expenses/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package it.polito.mad.team19.mad_expenses;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
